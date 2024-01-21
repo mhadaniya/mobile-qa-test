724adafb01d35c131cacf9d6184d7a81dafe1271
Victor Oliveira Santos <victor_oliveirasantos@hotmail.com>
2024-01-21 12:45:20 -0300

    feat: Melhora Testes Header/PickerLocation

--- src/__tests__/components/Header/Header.spec.tsx
+++ src/__tests__/components/Header/Header.spec.tsx
@@ -3,13 +3,9 @@ import React, { ReactElement } from 'react'
 import { render, fireEvent } from '@testing-library/react-native'
 import { ThemeProvider } from 'styled-components/native'
 
+import { IIcon } from '../../../@types/types'
 import { Default as HeaderStory } from '../../../components/Header/Header.stories'
 import theme from '../../../theme'
-
-interface IIcon {
-  icon: string
-  width: string
-}
 
 jest.mock('@assets/icons/icon', () => {
   return {
@@ -20,7 +16,7 @@ jest.mock('@assets/icons/icon', () => {
 })
 
 describe('Header', () => {
-  test('must have a Container with the correct testID', () => {
+  it('must have a Container with the correct testID', () => {
     const { getByTestId } = render(
       <ThemeProvider theme={theme}>
         <HeaderStory onOpen={() => {}} />
@@ -30,7 +26,7 @@ describe('Header', () => {
     expect(getByTestId('header')).toBeTruthy()
   })
 
-  test('must contain an icon with icon=bell and width=27', () => {
+  it('must contain an icon with icon=bell and width=27', () => {
     const { queryAllByTestId } = render(
       <ThemeProvider theme={theme}>
         <HeaderStory onOpen={() => {}} />
@@ -44,7 +40,7 @@ describe('Header', () => {
     expect(bellIcon).toBeTruthy()
   })
 
-  test('must render the PickerLocation with the onOpen prop', () => {
+  it('must render the PickerLocation with the onOpen prop', () => {
     const onOpenMock = jest.fn()
     const { getByTestId } = render(
       <ThemeProvider theme={theme}>
@@ -55,7 +51,7 @@ describe('Header', () => {
     expect(getByTestId('container')).toBeTruthy()
   })
 
-  test('must call onOpen when the PickerLocation is interacted with', () => {
+  it('must call onOpen when the PickerLocation is interacted with', () => {
     const onOpenMock = jest.fn()
     const { getByTestId } = render(
       <ThemeProvider theme={theme}>
--- src/__tests__/components/Picker/PickerLocation.spec.tsx
+++ src/__tests__/components/Picker/PickerLocation.spec.tsx
@@ -3,18 +3,29 @@ import * as React from 'react'
 import { render, fireEvent } from '@testing-library/react-native'
 import { ThemeProvider } from 'styled-components/native'
 
+import { IIcon } from '../../../@types/types'
 import { Default as PickerLocationStory } from '../../../components/Picker/PickerLocation.stories'
 import theme from '../../../theme'
 
 jest.mock('@assets/icons/icon', () => {
   return {
-    Icon: ({ icon }: any) => {
-      return <>{icon}</>
+    Icon: ({ icon, width }: IIcon): React.ReactElement => {
+      return <mock-icon testID='mock-icon' icon={icon} width={width} />
     }
   }
 })
 
 describe('PickerLocation component', () => {
+  it('must have a Container with the correct testID', () => {
+    const { getByTestId } = render(
+      <ThemeProvider theme={theme}>
+        <PickerLocationStory onOpen={() => {}} />
+      </ThemeProvider>
+    )
+
+    expect(getByTestId('container')).toBeTruthy()
+  })
+
   it('renders correctly', () => {
     const { getByText } = render(
       <ThemeProvider theme={theme}>
@@ -23,6 +34,24 @@ describe('PickerLocation component', () => {
     )
 
     expect(getByText('Location')).toBeTruthy()
+  })
+
+  it('must contain an icon location', () => {
+    const { queryAllByTestId } = render(
+      <ThemeProvider theme={theme}>
+        <PickerLocationStory onOpen={() => {}} />
+      </ThemeProvider>
+    )
+
+    const allIcons = queryAllByTestId('mock-icon')
+    const locationIcon = allIcons.find(
+      (icon) => icon.props.icon === 'location' && icon.props.width === '27'
+    )
+    const selectIcon = allIcons.find(
+      (icon) => icon.props.icon === 'select' && icon.props.width === '15'
+    )
+    expect(locationIcon).toBeTruthy()
+    expect(selectIcon).toBeTruthy()
   })
 
   it('calls onOpen prop when container is pressed', () => {
